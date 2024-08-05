e6d356f5c88f0c867b8f08e60f15cd7e5e15861c
Levi <levibezerra320@gmail.com>
2024-08-05 16:16:46 -0300

    Instalação do sequelize e também configuração do banco

---
+++ src/models/user.js
@@ -0,0 +1,41 @@
+// models/user.js
+const { DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/database'); // ajuste o caminho conforme sua estrutura
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  firstname: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  surname: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  timestamps: true,
+});
+
+// Antes de salvar o usuário, faça o hash da senha
+User.beforeCreate(async (user, options) => {
+  if (user.password) {
+    user.password = await bcrypt.hash(user.password, 10);
+  }
+});
+
+module.exports = User;
