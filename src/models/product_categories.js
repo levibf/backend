d03b856de03b15ab5ef100f7a9debc877836c2b4
JohnyMax42 <johnyernesto1997@gmail.com>
2024-08-07 15:04:31 -0300

    product_categories model

---
+++ src/models/product_categories.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require('sequelize');
+const Product = require('./models/product');
+const Category = require('./models/category');
+const sequelize = require('../config/database'); // ajuste o caminho conforme sua estrutura
+
+const ProductCategories = sequelize.define('ProductCategories', {
+    product_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: Product,
+            key: 'id'
+        }
+      },
+    category_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: Category,
+            key: 'id'
+        }
+    }
+}, {
+  timestamps: true,
+});
+
+Product.hasOne(ProductCategories);
+Category.hasOne(ProductCategories);
+ProductCategories.hasMany(Product, Category);
+
+module.exports = ProductCategories;
