97849302e5c901a85ce11bdedcd67045c5737612
Levi <levibezerra320@gmail.com>
2024-08-09 15:32:36 -0300

    Banco em nuvem aplicado ao projeto, deixado também a conexão com o banco local para testes

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,24 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('banco_teste', 'root', '1234', {
-    host: 'localhost',
-    port: 3307,
+const sequelize = new Sequelize('railway', 'root', 'XowovCXFTKLJzjvpdUHuCHAMkVDsNtLE', {
+    host: 'monorail.proxy.rlwy.net',
+    port: 56321,
     dialect: 'mysql',
     logging: false,
     dialectOptions: {
         connectTimeout: 10000
     }
 });
+
+// Banco locals
+// const sequelize = new Sequelize('banco_teste', 'root', '1234', {
+//     host: 'localhost',
+//     port: 3307,
+//     dialect: 'mysql',
+//     logging: false,
+//     dialectOptions: {
+//         connectTimeout: 10000
+//     }
+// });
 
 module.exports = sequelize;
