fab08d11e63f633c48fd1bf23bd9f30851fe2a81
JohnyMax42 <johnyernesto1997@gmail.com>
2024-08-09 16:40:25 -0300

    routes alterados

--- src/routes/categoriesRoutes.js
+++ src/routes/categoriesRoutes.js
@@ -1,23 +1,23 @@
 const express = require('express');
-const router = express.Router();
+const categoriesRouter = express.Router();
 const categoriesController = require('../controllers/categoriesController');
 
 // Rota para buscar todos as categorias
-router.get('/', categoriesController.getCategories);
+categoriesRouter.get('/', categoriesController.getCategories);
 
 //Rota para buscar categoria por ID
-router.get('/:id', categoriesController.getCategoryById);
+categoriesRouter.get('/:id', categoriesController.getCategoryById);
 
 // Rota POST (/v1/category) para cadastro de uma categoria
-router.post('/', categoriesController.createCategory);
+categoriesRouter.post('/', categoriesController.createCategory);
 
 // Rota PUT (/v1/category/:id) para atualizar dados da categoria
-router.put('/:id', categoriesController.updateCategory);
+categoriesRouter.put('/:id', categoriesController.updateCategory);
 
 // Rota DELETE (/v1/category/:id) para excluir uma categoria
-router.delete('/:id', categoriesController.deleteCategory);
+categoriesRouter.delete('/:id', categoriesController.deleteCategory);
 
 // Estrutura das rotas
 // Routes > Controller > Services > Model
 
-module.exports = router;
+module.exports = categoriesRouter;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,23 +1,23 @@
 const express = require('express');
-const router = express.Router();
+const userRouter = express.Router();
 const userController = require('../controllers/userController');
 
 // Rota para buscar todos os usuários
-router.get('/', userController.getUsers);
+userRouter.get('/', userController.getUsers);
 
 //Rota para buscar por usuário por ID
-router.get('/:id', userController.getUserById);
+userRouter.get('/:id', userController.getUserById);
 
 // Rota POST (/v1/user) para cadastro de usuário
-router.post('/', userController.createUser);
+userRouter.post('/', userController.createUser);
 
 // Rota PUT (/v1/user/:id) para atualizar dados do usuário
-router.put('/:id', userController.updateUser);
+userRouter.put('/:id', userController.updateUser);
 
 // Rota DELETE (/v1/user/:id) para excluir um usuário
-router.delete('/:id', userController.deleteUser);
+userRouter.delete('/:id', userController.deleteUser);
 
 // Estrutura das rotas
 // Routes > Controller > Services > Model
 
-module.exports = router;
+module.exports = userRouter;
